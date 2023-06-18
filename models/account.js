989fe0c64a0b472a95841d7ffc3f142b4f3c1ae4
NghiaVNM <duongphanhieunghia2003@gmail.com>
2023-06-18 17:52:02 +0700

    update

--- models/account.js
+++ models/account.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/MyDB', {
+/*mongoose.connect('mongodb://127.0.0.1:27017/MyDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+});*/
+mongoose.connect('mongodb+srv://nghia:nghia123@test.y6lecmv.mongodb.net/');
+mongoose.connection
+.once('open', function() {
+    console.log('MongoDB running');
+})
+.on('error', function (err) {
+    console.log(err);
+})
 
 const Schema = mongoose.Schema;
 
